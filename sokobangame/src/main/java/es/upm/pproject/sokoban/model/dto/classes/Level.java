fb0a684a0d026a9ab508a97a854365dab7830d68
Mateo Cuñarro Alfonsín <mateo.cunarro@alumnos.upm.es>
2026-05-12 22:13:51 +0200

    Añadida interfaz de Level, y atributos de Level.java

--- sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/Level.java
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/Level.java
@@ -2,4 +2,20 @@ package es.upm.pproject.sokoban.model.dto.classes;
 
 public class Level {
 
+    private Square capaInf[][];
+    private Square capaSup[][];
+    private Score puntuacion;
+    private int filas;
+    private String nombre;
+    private int Columnas;
+
+
+    
+    
+
+
+
+    
+
+
 }
---
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/interfaces/Level.java
@@ -0,0 +1,23 @@
+package es.upm.pproject.sokoban.model.dto.interfaces;
+
+import es.upm.pproject.sokoban.model.dto.classes.Score;
+import es.upm.pproject.sokoban.model.dto.classes.Square;
+
+public interface Level {
+    
+    public Square[][] getCapaInf();
+    public Square[][] getCapaSup();
+    public Square[][] setCapaInf();
+    public Square[][] setCapaSup();
+    public String getNombre();
+    public int getFilas();
+    public int getColumnas();
+    public Score getPuntuacion();
+    public String setNombre();
+    public int setFilas();
+    public int setColumnas();
+    public Score setPuntuacion();
+    
+
+
+}
