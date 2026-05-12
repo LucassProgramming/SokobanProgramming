bfda59c40a1d06d33d5e0e0b7e583977ffc5c12f
BENÍTEZ MAIDANA, LUCAS DANIEL <lucas.bmaidana@alumnos.upm.es>
2026-05-12 21:29:34 +0200

    Test para SquareTest

--- sokobangame/src/test/java/es/upm/pproject/sokoban/SquareTest.java
+++ sokobangame/src/test/java/es/upm/pproject/sokoban/SquareTest.java
@@ -1,5 +1,42 @@
 package es.upm.pproject.sokoban;
 
-public class SquareTest {
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
 
+import es.upm.pproject.sokoban.model.dto.classes.Square;
+
+public class SquareTest {
+    @Test
+    void SquareCoordenadaXCorrecta(){
+        Square square = new Square(2, 5);
+        assertEquals(2, square.getX());
+    }
+
+    @Test
+    void SquareCoordenadaYCorrecta(){
+        Square square = new Square(2, 5);
+        assertEquals(5, square.getY());
+    }
+    @Test
+    void SquareCoordenadaXIncorrecta(){
+        Square square = new Square(2, 5);
+        assertEquals(3, square.getX());
+    }
+    @Test
+    void SquareCoordenadaYIncorrecta(){
+        Square square = new Square(2, 5);
+        assertEquals(6, square.getY());
+    }
+    @Test
+    void deberiaGuardarCoordenadasCero(){
+        Square square = new Square(0, 0);
+        assertEquals(0, square.getX());
+        assertEquals(0, square.getY());
+    }
+    @Test 
+    void deberiaGuardarCoordenadasNegativas(){
+        Square square = new Square(-1, -1);
+        assertEquals(-1, square.getX());
+        assertEquals(-1, square.getY());
+    }
 }
