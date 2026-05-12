13ef7864eec3b39c30561fdf2a1762afb731430d
BENÍTEZ MAIDANA, LUCAS DANIEL <lucas.bmaidana@alumnos.upm.es>
2026-05-12 21:43:45 +0200

    clase Wall y tests

--- sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/Wall.java
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/Wall.java
@@ -1,5 +1,7 @@
 package es.upm.pproject.sokoban.model.dto.classes;
 
 public class Wall extends Square {
-
+    public Wall(int x, int y){
+        super(x,y);
+    }
 }
---
+++ sokobangame/src/test/java/es/upm/pproject/sokoban/WallTest.java
@@ -0,0 +1,20 @@
+package es.upm.pproject.sokoban;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.Test;
+
+import es.upm.pproject.sokoban.model.dto.classes.Wall;
+
+
+public class WallTest {
+    @Test
+    void WallCoordenadaXCorrecta(){
+        Wall wall = new Wall(2, 5);
+        assertEquals(2, wall.getX());
+    }
+    @Test
+    void WallCoordenadaYCorrecta(){
+        Wall wall = new Wall(2, 5);
+        assertEquals(5, wall.getY());
+    }
+}
