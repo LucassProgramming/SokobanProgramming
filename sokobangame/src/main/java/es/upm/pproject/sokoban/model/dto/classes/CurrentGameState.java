a3c70556de1d7bcdbbcfe3a29f1c5f359350bf31
denisandrei-byte <vasilicarodica6@gmail.com>
2026-05-13 11:24:12 +0200

    Borrado Game mezclado con CurrentGameState

--- sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/CurrentGameState.java
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/CurrentGameState.java
@@ -1,5 +1,19 @@
 package es.upm.pproject.sokoban.model.dto.classes;
 
 public class CurrentGameState {
+    private Level [] arrayLevels;
+    private int index = 0;
+    private Level current;
 
+    public CurrentGameState(){
+        arrayLevels = new Level[99];
+    }
+
+    public Level[] getArray(){
+        return arrayLevels;
+    }
+    public void añadirLevel(Level level){
+       arrayLevels[index] = level;
+       index++;
+    }
 }
