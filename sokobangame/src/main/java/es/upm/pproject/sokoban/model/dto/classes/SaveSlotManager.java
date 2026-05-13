fbda61f84e5645dec728927d198b98c67daec758
BENÍTEZ MAIDANA, LUCAS DANIEL <lucas.bmaidana@alumnos.upm.es>
2026-05-13 23:53:47 +0200

    Implementados existeSlot, borrarSlot y cargarPartida + tests respectivos

--- sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/SaveSlotManager.java
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/SaveSlotManager.java
@@ -1,14 +1,16 @@
 package es.upm.pproject.sokoban.model.dto.classes;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.io.Serializable;
 
 import es.upm.pproject.sokoban.model.dto.interfaces.ICurrentGameState;
 import es.upm.pproject.sokoban.model.dto.interfaces.ISaveSlotManager;
 
-public class SaveSlotManager implements ISaveSlotManager, Serializable{
+public class SaveSlotManager implements ISaveSlotManager{
     @Override
     public void guardarPartida(ICurrentGameState estado, int slot) throws IOException{
         
@@ -23,11 +25,30 @@ public class SaveSlotManager implements ISaveSlotManager, Serializable{
         archivo.close();
     }
     @Override
-    public ICurrentGameState cargarPartida(int slot)  throws IOException, ClassNotFoundException{return null;}
+    public ICurrentGameState cargarPartida(int slot)  throws IOException, ClassNotFoundException{
+        String nombreArchivo = "slot" + slot + ".dat";
+        //Abre el archivo del slot indica para leer bytes
+        FileInputStream archivo = new FileInputStream(nombreArchivo);
+        // Convierte los bytes en un objeto
+        ObjectInputStream entrada = new ObjectInputStream(archivo);
+        // Lee el objeto del archivo
+        ICurrentGameState estado = (ICurrentGameState) entrada.readObject();
+        entrada.close();
+        archivo.close();
+        return estado;
+    }
     @Override
-    public boolean existeSlot(int slot){return false;}
+    public boolean existeSlot(int slot){
+        String nombreArchivo = "slot" + slot + ".dat";
+        File archivo = new File(nombreArchivo);
+        return archivo.exists();
+    }
     @Override
-    public void borrarSlot(int slot){}
-
-
+    public void borrarSlot(int slot){
+        String nombreArchivo = "slot" + slot + ".dat";
+        File archivo = new File(nombreArchivo);
+        if (archivo.exists()) {
+            archivo.delete();
+        }
+    }
 }
