4ed8597a801355208361a23510762991156d08a8
denisandrei-byte <vasilicarodica6@gmail.com>
2026-05-20 10:09:25 +0200

    Ajuste

--- sokobangame/src/test/java/es/upm/pproject/sokoban/CharacterManagerTest.java
+++ sokobangame/src/test/java/es/upm/pproject/sokoban/CharacterManagerTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import org.junit.jupiter.api.Test;
 
 import es.upm.pproject.sokoban.model.dto.classes.*;
-import es.upm.pproject.sokoban.model.dto.interfaces.ILevel;
 
 public class CharacterManagerTest {
     
