86ff70142359b38c7a5eaef2ce05bf7f875aceca
Mateo Cuñarro Alfonsín <mateo.cunarro@alumnos.upm.es>
2026-06-07 22:51:43 +0200

    Cambios en LevelFileReader para Sonar

--- sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/LevelFileReader.java
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/model/dto/classes/LevelFileReader.java
@@ -21,108 +21,42 @@ public class LevelFileReader {
 
     private LevelFileReader(){}
 
-    public static Level crearNivel(String archivo){
-        PlayableCharacter caracter = null;
-        ArrayList<String> lineas = new ArrayList<>();
-        //Contadores para las distintas validaciones
-        int numBoxes = 0;
-        int numGoals = 0;
-        int numPlayableCharacters = 0;
+    public static Level crearNivel(String archivo) {
 
-         //Le ponemos la / manualmente para que lea la ruta de menu controller bien.
-        String ruta = archivo.startsWith("/") ? archivo : "/" + archivo;
-        InputStream stream = LevelFileReader.class.getResourceAsStream(ruta);
+    String ruta = archivo.startsWith("/") ? archivo : "/" + archivo;
+    InputStream stream = LevelFileReader.class.getResourceAsStream(ruta);
 
-        if (stream == null) {
-            throw new LevelDoesntExistException(archivo);
+    if (stream == null) {
+        throw new LevelDoesntExistException(archivo);
+    }
+
+    List<String> lineas = leerLineas(stream);
+
+    String nombre = lineas.get(0);
+    String[] division = lineas.get(1).split("\\s+");
+
+    int filas = Integer.parseInt(division[0]);
+    int columnas = Integer.parseInt(division[1]);
+
+    Square[][] capaInf = new Square[filas][columnas];
+    Square[][] capaSup = new Square[filas][columnas];
+
+    DatosNivel datos = new DatosNivel();
+
+    for (int i = 0; i < filas; i++) {
+        String fila = lineas.get(i + 2);
+
+        for (int j = 0; j < columnas; j++) {
+            char celda = j < fila.length() ? fila.charAt(j) :' ';
+
+            procesarCelda(celda, i, j, capaInf, capaSup, datos);
         }
+    }
 
-        try (BufferedReader lector = new BufferedReader(new InputStreamReader(stream))) {
+    validarNivel(nombre, datos.numGoals, datos.numBoxes, datos.numPlayableCharacters);
 
-            String linea;
-
-            while ((linea = lector.readLine()) != null) {
-                lineas.add(linea);
-            }
-        
-        } catch (Exception e){
-            logger.info("Error en lectura {}", e.getMessage());
-
-        }
-
-        String nombre = lineas.get(0);
-        String[] division = lineas.get(1).split("\\s+");
-
-        int filas = Integer.parseInt(division[0]);
-        int columnas = Integer.parseInt(division[1]);
-        
-
-
-        Square [][] capaInf = new Square[filas][columnas];
-        Square [][] capaSup = new Square[filas][columnas];
-
-        for(int i=0; i< filas; i++){
-            String comienzoNivelArchivo = lineas.get(2+i);
-            for(int j=0; j< columnas;j++){
-                char celda = j < comienzoNivelArchivo.length() ? comienzoNivelArchivo.charAt(j) : ' ';
-                switch (celda) {
-                    case '+':
-                        capaInf[i][j] = new Wall(i,j);
-                        break;
-
-                    case '*':
-                        capaInf[i][j] = new Goal(i,j);
-
-                        
-                        numGoals++;
-
-                        break;
-                    
-                    case '#':
-                        capaSup[i][j] = new Box(i, j);
-                        capaInf[i][j] = new Square(i, j);
-
-
-                        numBoxes++;
-
-                        break;
-
-                    case 'W':    
-                        caracter = new PlayableCharacter(i, j);
-                        capaSup[i][j] = caracter;
-                        capaInf[i][j] = new Square(i, j);
-
-                        numPlayableCharacters++;
-
-                        break;
-                
-                    default:
-                        capaInf[i][j] = new Square(i, j);
-                        break;
-                }
-
-                }
-            }
-            // Condiciones mínimas del juego
-            if(numBoxes == 0){
-                throw new CajaNotFoundInLevelException(nombre);
-            }
-
-            if(numGoals == 0){
-                throw new GoalNotFoundInLevelException(nombre);
-            }
-
-            if (numPlayableCharacters != 1) {
-                throw new PlayableCharacterNotFoundInLevelException(nombre);
-            }
-
-            if(numBoxes != numGoals){
-                throw new GoalsAndBoxesArentEqualsException(nombre);
-            }
-
-            return new Level(nombre,filas,columnas,capaInf,capaSup,new Score(),caracter);
-        }
-        
+    return new Level(nombre, filas, columnas, capaInf, capaSup, new Score(), datos.caracter);
+}
     public static List<Level> cargarTodosLosNiveles(){
         List<Level> niveles = new ArrayList<>();
         int contador=1;
@@ -139,4 +73,85 @@ public class LevelFileReader {
         }
             return niveles;
     }
+
+    private static List<String> leerLineas(InputStream stream) {
+
+    List<String> lineas = new ArrayList<>();
+
+    try (BufferedReader lector =
+                 new BufferedReader(new InputStreamReader(stream))) {
+
+        String linea;
+
+        while ((linea = lector.readLine()) != null) {
+            lineas.add(linea);
+        }
+
+    } catch (Exception e) {
+        logger.info("Error en lectura {}", e.getMessage());
+    }
+
+    return lineas;
+}
+
+    private static void validarNivel(String nombre,int numGoals,int numBoxes, int numPlayableCharacters){
+            
+            if(numBoxes == 0){
+                throw new CajaNotFoundInLevelException(nombre);
+            }
+
+            if(numGoals == 0){
+                throw new GoalNotFoundInLevelException(nombre);
+            }
+
+            if (numPlayableCharacters != 1) {
+                throw new PlayableCharacterNotFoundInLevelException(nombre);
+            }
+
+            if(numBoxes != numGoals){
+                throw new GoalsAndBoxesArentEqualsException(nombre);
+            }
+
+    }
+
+    private static void procesarCelda(char celda, int i, int j,Square[][] capaInf,Square[][] capaSup,DatosNivel datos){
+        
+        switch (celda) {
+
+        case '+':
+            capaInf[i][j] = new Wall(i, j);
+            break;
+
+        case '*':
+            capaInf[i][j] = new Goal(i, j);
+            datos.numGoals++;
+            break;
+
+        case '#':
+            capaSup[i][j] = new Box(i, j);
+            capaInf[i][j] = new Square(i, j);
+            datos.numBoxes++;
+            break;
+
+        case 'W':
+            datos.caracter = new PlayableCharacter(i, j);
+            capaSup[i][j] = datos.caracter;
+            capaInf[i][j] = new Square(i, j);
+            datos.numPlayableCharacters++;
+            break;
+
+        default:
+            capaInf[i][j] = new Square(i, j);
+            break;
+        }
+    }
+
+    private static class DatosNivel {
+
+    int numBoxes;
+    int numGoals;
+    int numPlayableCharacters;
+    PlayableCharacter caracter;
+}
+
 }
