bcb773007b0fdaa36697d689df6477e252b118ab
denisandrei-byte <vasilicarodica6@gmail.com>
2026-06-07 12:54:02 +0200

    Cambios de variables en la clase LevelFileReader

--- sokobangame/src/test/java/es/upm/pproject/sokoban/model/dto/classes/BoxManagerTest.java
+++ sokobangame/src/test/java/es/upm/pproject/sokoban/model/dto/classes/BoxManagerTest.java
@@ -5,8 +5,6 @@ import static org.junit.jupiter.api.Assertions.*;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
-import java.awt.Color;
-
 public class BoxManagerTest {
 
     private Square[][] capaInf;
