cacbe3a1b9b433164d098a8f543db6542bdbea08
230226 <jiaxu1006@gmail.com>
2026-06-12 14:13:32 +0200

    arreglado el code smell en App.java

--- sokobangame/src/main/java/es/upm/pproject/sokoban/App.java
+++ sokobangame/src/main/java/es/upm/pproject/sokoban/App.java
@@ -10,7 +10,7 @@ public class App extends Application {
     @Override
     public void start(Stage stage) {
         MenuController controller = new MenuController(stage);
-        MainMenuView mainMenuView = new MainMenuView(stage, controller);
+        new MainMenuView(stage, controller);
     }
 
     public static void main(String[] args) {
