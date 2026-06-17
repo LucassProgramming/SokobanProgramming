a759be6f4d8e12d74fe6e3d562bcba56a4b7e573
BENÍTEZ MAIDANA, LUCAS DANIEL <lucas.bmaidana@alumnos.upm.es>
2026-06-17 16:14:35 +0200

    Evitar fallo al iniciar musica en entornos sin soporte multimedia

--- src/main/java/es/upm/pproject/sokoban/view/MusicView.java
+++ src/main/java/es/upm/pproject/sokoban/view/MusicView.java
@@ -1,35 +1,50 @@
 package es.upm.pproject.sokoban.view;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 
 
 public class MusicView{
+    private static final Logger LOGGER = Logger.getLogger(MusicView.class.getName());
     private static MediaPlayer mediaPlayer;
  
     private MusicView(){
     }
 
     public static void start(String musicURL) {
-        double volume = (mediaPlayer != null) ? mediaPlayer.getVolume() : 1.0;
-        Media media = new Media(musicURL);
-        mediaPlayer = new MediaPlayer(media);
-        mediaPlayer.setVolume(volume);
-        mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
-        mediaPlayer.play();
+        try {
+            double volume = (mediaPlayer != null) ? mediaPlayer.getVolume() : 1.0;
+            Media media = new Media(musicURL);
+            mediaPlayer = new MediaPlayer(media);
+            mediaPlayer.setVolume(volume);
+            mediaPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+            mediaPlayer.play();
+        } catch (RuntimeException e) {
+            mediaPlayer = null;
+            LOGGER.log(Level.WARNING, "No se pudo iniciar la musica", e);
+        }
     }
 
-public static void stop(){
-    if(mediaPlayer!=null) mediaPlayer.pause();
-}
-public static void turnUp(){
-    double volumen = mediaPlayer.getVolume();
-    if((volumen) < 1.0) mediaPlayer.setVolume(volumen + 0.1); 
-}
-public static void turnDown(){
-    double volumen = mediaPlayer.getVolume();
-    if((volumen) > 0.0) mediaPlayer.setVolume(volumen - 0.1); 
-}
+    public static void stop(){
+        if(mediaPlayer!=null) mediaPlayer.pause();
+    }
+
+    public static void turnUp(){
+        if(mediaPlayer!=null){
+            double volumen = mediaPlayer.getVolume();
+            if((volumen) < 1.0) mediaPlayer.setVolume(volumen + 0.1);
+        }
+    }
+
+    public static void turnDown(){
+        if(mediaPlayer!=null){
+            double volumen = mediaPlayer.getVolume();
+            if((volumen) > 0.0) mediaPlayer.setVolume(volumen - 0.1);
+        }
+    }
 
 
 }
